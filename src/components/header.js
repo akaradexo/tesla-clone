eb3edb761ab846e8fa6142ff352e4056a8617a4a
Ashutosh Karade <69238714+xR0v3Rx@users.noreply.github.com>
2021-09-19 18:29:20 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -14,7 +14,7 @@ function Header() {
   return (
     <Container>
       <a>
-        <img src="/images/logo.svg"/>
+        <img src="/images/logo.svg" alt="logo"/>
       </a>
       <Menu>
         {cars && cars.map((car, index)=>(
