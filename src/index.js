68957b1937672f9576d18f68f18f9aa1f84f061a
Ashutosh Karade <69238714+xR0v3Rx@users.noreply.github.com>
2021-09-25 13:16:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 
 ReactDOM.render(
-  <Provider store={store}>
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-  </Provider>,
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
